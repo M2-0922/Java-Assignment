0d5432076e47156e64cc553ae1b50259f0538784
Soma-dev0808 <ec.fate.gt@gmail.com>
2022-10-20 09:16:59 -0700

    soma ikeda q1

--- Assignment-1/src/A1.java
+++ Assignment-1/src/A1.java
@@ -1,5 +1,37 @@
-public class A1{
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class A1 {
     public static void main(String[] args) {
         // write your code here
+        int value = 0;
+        Random rd = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(1 + rd.nextInt(50));
+        }
+
+        System.out.println("Array" + list);
+        System.out.print("Input number");
+
+        try {
+            value = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("You must input integer");
+        } finally {
+            scanner.close();
+        }
+
+        if (list.contains(value)) {
+            System.out.println(value + " is included.");
+        } else {
+            System.out.println(value + " is not included.");
+        }
+
     }
 }
