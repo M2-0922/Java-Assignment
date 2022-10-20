f5c0ce46e4623f848d0359aa1e909719f490e71f
Soma-dev0808 <ec.fate.gt@gmail.com>
2022-10-20 09:43:11 -0700

    soma ikeda q3

--- Assignment-1/src/A3.java
+++ Assignment-1/src/A3.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Random;
+
 public class A3 {
     public static void main(String[] args) {
         // write your code here
+        record Car(String make, String model, Integer year) {
+        }
+
+        ArrayList<Car> list = new ArrayList<Car>();
+        Random rd = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(new Car("Toyota" + rd.nextInt(20), "sample" + rd.nextInt(20), rd.nextInt(2020)));
+        }
+
+        System.out.println(list);
+        list.sort(Comparator.comparing(Car::year));
+
+        list.forEach(System.out::println);
+
     }
 }
